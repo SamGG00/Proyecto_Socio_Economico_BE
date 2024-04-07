c63c473d13384602485be5d3613a4fa7c23038b6
Juan Andrés <juan.andresgiraldo@hotmail.com>
2024-04-07 14:52:15 -0500

    primer commit

---
+++ src/controllers/index.ts
@@ -0,0 +1,8 @@
+export * from './apoyo-socioeconomico.controller';
+export * from './convocatoria-apoyo-socioeconomico.controller';
+export * from './convocatoria.controller';
+export * from './estudiante-apoyo-controller.controller';
+export * from './estudiante-controller.controller';
+export * from './ping.controller';
+export * from './resultado-convocatoria.controller';
+
